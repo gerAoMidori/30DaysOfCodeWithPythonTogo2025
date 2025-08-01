fb2a60e75833546f3f3ba2d87c4992aa8458ec16
juliteGarden <111869677+gerAoMidori@users.noreply.github.com>
2025-08-01 07:44:20 +0000

    Day 10

--- Day 10/level2.py
+++ Day 10/level2.py
@@ -3,18 +3,17 @@ a = 0
 for i in range(1, 101):
     a += i
 
-print("Sum of numbers from 1 to 100 :", a)
+print(f"The sum of all numbers is {a}.")
 
 #2
 b = 0
 for i in range(1, 101):
     if i % 2 == 0:
         b += i
-print("Sum of even numbers from 1 to 100 :", b)
+print(f"The sum of all evens is {b}.")
 
-#3
 c = 0  
 for i in range(1, 101):
     if i % 2 != 0:
         c += i
-print("Sum of odd numbers from 1 to 100 :", c)
+print(f"The sum of all odds is {c}.")
